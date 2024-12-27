8c0eb23c2e939402da37379a39eeb68bffb8d1f1
Wei-Chen Li <williamrob104@gmail.com>
2024-12-27 09:57:25 +0800

    Add split Bregman method

---
+++ algorithms/sparse_ADMM.m
@@ -0,0 +1,65 @@
+% -------------------------------------------------------------------------
+% x_hat = sparse_ADMM(Phi, y, lambda)
+%
+% Convex optimization using ADMM for sparse vector recovery
+% -------------------------------------------------------------------------
+% Phi: M×N array
+% y:   M×1 array
+% x_hat: N×1 vector
+%--------------------------------------------------------------------------
+function x = sparse_ADMM(Phi, y, lambda, varargin)
+
+p = inputParser;
+addParameter(p, 'MaxIterations', 500, @isscalar)
+addParameter(p, 'x_init', [], @isvector)
+parse(p, varargin{:});
+
+max_iters = p.Results.MaxIterations;
+x_init = p.Results.x_init;
+clear p
+
+[M,N] = size(Phi);
+
+if isempty(x_init)
+    x = 0.5 * ones(N,1);
+else
+    x = x_init;
+end
+z = x;
+nu = zeros(N,1);
+
+rho = 0.1;
+
+Gram_Phi = Phi' * Phi;
+I = speye(N);
+A = 2 * Gram_Phi + rho * I;
+A = decomposition(A, 'chol');
+
+for iter = 1:max_iters
+    x_prev = x;
+
+    x = A \ (2 * Phi' * y + rho * z - nu);
+    z = softThresholding(x + 1/rho * nu, lambda/rho);
+    nu = nu + rho * (x - z);
+
+    r_dual = 2 * (Gram_Phi * x - Phi' * y) + nu;
+    r_prim = x - z;
+    c = sqrt( (norm(r_prim,Inf) / max([norm(x,Inf) norm(z,Inf)])) / ...
+              (norm(r_dual,Inf) / max([norm(2*Gram_Phi*x,Inf) norm(2*Phi'*y,Inf) norm(nu,Inf)])) );
+    if c < 0.2 || c > 5
+        rho = c * rho;
+        A = 2 * Gram_Phi + rho * I;
+        A = decomposition(A, 'chol');
+    end
+
+    if norm(x - x_prev, Inf) < 1e-10
+        break
+    end
+end
+
+end
+
+
+function y = softThresholding(x, thresh)
+y = sign(x) .* max(abs(x) - thresh, 0);
+end
